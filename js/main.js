3931afd87c4ef0c9e4859af7809fffc0bf9f3c45
mostafataw159 <152802283+mostafataw159@users.noreply.github.com>
2023-12-06 19:54:29 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// camel case
+
+
+
+var x =document.getElementById("hello");
+x.innerHTML = "this is change to js";
+x.style.color = "#f44";
+x.style.textAlign = "center";
+
+//console
+
+console.log("mostafajds");
+
+//alert
+// alert(474);
+
+
+// document.write(47474);
+
+
+
+// veriable
+
+// var x = 47;
+
+// x = 8;
+// y = 5;
+
+// z = y+x;
+
+// console.log(z);
+
+// types
+
+x = "khald"; //string
+
+y = 47;    // number
+
+z = false;   //coolean
+
+names = ["ahmad","khald",44,true];   //arrey
+console.log(names [3]);      //object
+ 
+// person
+
+var person = {
+    firstName : "ahmed",
+
+    lastName :"hkald",
+
+    age : 47,
+
+    anOwner :true
+}
+console.log(typeof person)    //object
